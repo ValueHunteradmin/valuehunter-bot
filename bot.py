283e6db339b39484133f4b23835df85cb0e36416
ValueHunteradmin <axilleasziovas123@gmail.com>
2026-02-27 17:51:31 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import os
 import threading
 import time
 
-# 🔑 ΒΑΛΕ ΤΑ ΔΙΚΑ ΣΟΥ
+
 TOKEN = os.environ.get("BOT_TOKEN")  # FIXED
 ADMIN_CHANNEL_ID = -1003705705673
 API_KEY = "2f8c79b66ceed85aaf20322308f11e5a"
